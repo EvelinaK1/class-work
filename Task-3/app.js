9b062df7fca0cf628e73410d1a126e86a08a0e7a
EvelinaK1 <evelinago.pro@gmail.com>
2024-01-25 10:31:34 +0300

    Выполнили работу с многомерными массивами

---
+++ Task-3/app.js
@@ -0,0 +1,3 @@
+let companies = [['Nissan', 'Audi', 'Mercedes-Benz', 'BMW', 'Volvo'], ['Apple', 'Samsung', 'Git', 'Google', 'Reddit'], ['Dior', 'Gucci', 'Chanel', 'Versace', 'Louis Vuitton'], ['McDonalsds', 'KFC', 'InNout', 'Starbucks', 'Burger King']]
+console.log(companies)
+console.log(companies[2])
