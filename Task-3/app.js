b18438831596b6fe06803f85935a526ed1615804
EvelinaK1 <evelinago.pro@gmail.com>
2024-01-25 13:41:56 +0300

    Поработали с массивами

--- Task-3/app.js
+++ Task-3/app.js
@@ -1,3 +1,23 @@
-let companies = [['Nissan', 'Audi', 'Mercedes-Benz', 'BMW', 'Volvo'], ['Apple', 'Samsung', 'Git', 'Google', 'Reddit'], ['Dior', 'Gucci', 'Chanel', 'Versace', 'Louis Vuitton'], ['McDonalsds', 'KFC', 'InNout', 'Starbucks', 'Burger King']]
-console.log(companies)
-console.log(companies[2])
+const colleges = [
+// Студент
+{
+    StudentName: "Ivan",
+    lastName: "Potter",
+    CourseNumber: 3,
+    Age: 18,
+    Hobby: ["Play computer games", "Reading", "Watch TV shows"],
+    
+},
+// Преподователь
+{
+    name: "George",
+    lastName: "Petrov",
+    age: 63,
+    Disciplines: ["Math", "Chemistry", "NativeLanguage"],
+    Number_of_lectures: 5,
+}
+
+]
+
+console.log(colleges[0]["StudentName"], colleges[0]["Age"])
+console.log(colleges[1]["name"], colleges[1]["Disciplines"])
