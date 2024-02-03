bfcb80dfdc9747ac663643a3eb0e32e3f8443576
EvelinaK1 <evelinago.pro@gmail.com>
2024-02-03 17:49:20 +0300

    Задание 3

--- Task-9/main.js
+++ Task-9/main.js
@@ -1,23 +1,26 @@
 class ArraySum {
+    
+    inputNumbers;
+
     constructor(numbers) {
-        this.imputNumbers = numbers
+        this.inputNumbers = numbers
     }
 
     calculateSum() {
         let sum = 0
-        for( let i = 0; i <= this.imputNumbers.lenght; i++ ) {
-            sum +- this.imputNumbers[i]
+        for( let i = 0; i < this.inputNumbers.length; i++) {
+            sum += this.inputNumbers[i]
         }
 
         return sum
     }
 
     print() {
-        const sum = this.calculateSum
-        console.log(sum)
+        const sum2 = this.calculateSum()
+        console.log(sum2)
     }
 }
 
-const inputNumbers = [1,2,3,4,5,6,7,8,9]
+const inputNumbers = [1,2,3,4,5,]
 const result = new ArraySum(inputNumbers)
 result.print();
